5f1e100b1f49f138045cf90e968b6b9c96ff2d5d
RainyFluff <75020737+RainyFluff@users.noreply.github.com>
2023-10-05 10:36:17 +0200

    Laps system done and maps worked on
    Script to manage checkpoints and laps done, simple but working And the maps have recieved more detail.

--- KarioMart (Assignment 1)/Assets/Scripts/LapCode.cs
+++ KarioMart (Assignment 1)/Assets/Scripts/LapCode.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SceneManagement;
 public class LapCode : MonoBehaviour
 {
+    //A list to manage all the checkpoints making this code a bit modular for several levels
+    public List<Collider> checkpoints;
+    private int checkpointNumber;
+    private int checkpointsGoneThrough;
+    public Collider start;
+    private bool isFinished = false;
+    public TextMeshProUGUI checkpointsText;
+    //Includes postprocessing and correct win text "Player1" "Player2"
+    public GameObject winScreen;
+    public GameObject lapFinishedText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        checkpointNumber = checkpoints.Count;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (checkpointNumber == checkpointsGoneThrough)
+        {
+            lapFinishedText.SetActive(true);
+            isFinished = true;
+        }
+        checkpointsText.text = checkpointsGoneThrough.ToString()+ "/" +checkpointNumber.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Checkpoint")
+        {
+            checkpointsGoneThrough++;
+        }
+
+        if (other.tag == "Finish" && isFinished)
+        {
+            StartCoroutine(finish());
+        }
+    }
+
+    IEnumerator finish()
+    {
+        winScreen.SetActive(true);
+        Time.timeScale = 0.2f;
+        yield return new WaitForSecondsRealtime(5);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
