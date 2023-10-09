6e95fd9afcc58a96c36d3178fcf614e8bbc747eb
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2023-10-09 22:31:52 +0200

    Bomb first code
    First part of the bomb code implemented

--- KarioMart (Assignment 1)/Assets/Scripts/PickUps.cs
+++ KarioMart (Assignment 1)/Assets/Scripts/PickUps.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,45 @@ using UnityEngine;
 public class PickUps : MonoBehaviour
 {
     private bool hasBomb;
-    public float bombForce;
+    public float bombForce = 100;
+    public float bombRadius = 10;
     public GameObject bombUISprite;
-    void Start()
-    {
-        //Get all components neccesary
-    }
+    private RaycastHit hit;
+    //debug vars
+    //public Transform bombPos;
 
     // Update is called once per frame
     void Update()
     {
+        if (hasBomb)
+        {
+            bombUISprite.SetActive(true);
+        }
+
+        if (hasBomb && Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(Collider collider in Physics.OverlapSphere(transform.position, bombRadius))
+            {
+                if (collider.GetComponent<Rigidbody>() != null)
+                {
+                    Rigidbody targetRB = collider.GetComponent<Rigidbody>();
+                    targetRB.AddExplosionForce(bombForce, transform.position, bombRadius);
+                }
+                Debug.Log(collider.transform);
+            }
+            hasBomb = false;
+        }
         //bomb uses rb.addexplosiveforce
         //bomb ui sprite activate and deactivate
         //pickup on the map being respawnable
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bomb")
+        {
+            Destroy(other.gameObject);
+            hasBomb = true;
+        }
     }
 }
